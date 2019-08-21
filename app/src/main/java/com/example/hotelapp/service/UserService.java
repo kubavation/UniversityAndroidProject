a4a5a95b20214d9f5c662c5c33ea453a761cd5f8
kubavation <kubavation@wp.pl>
2019-08-21 19:28:42 +0200

    added userService

--- app/src/main/java/com/example/hotelapp/service/UserService.java
+++ app/src/main/java/com/example/hotelapp/service/UserService.java
@@ -1,7 +1,35 @@
 package com.example.hotelapp.service;
 
+import com.example.hotelapp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
 
+    private static List<User> users;
 
+    static {
+        users = new ArrayList<>();
+        users.add(new User("test","test"));
+    }
 
+    public static void addUserIfNotExists(User user) {
+        if(isUsernameExists(user)) {
+            throw new RuntimeException("This username is already taken");
+        }
+        users.add(user);
+    }
+
+    private static boolean isUsernameExists(User user) {
+        for (User u : users) {
+            if (u.getUsername().equals(user.getUsername()))
+                return true;
+        }
+        return false;
+    }
+
+    public static List<User> getUsers() {
+        return users;
+    }
 }
