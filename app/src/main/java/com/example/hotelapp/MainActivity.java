f18d3e585e49c595d0c5b5abad6cf16ee13ad2ca
kubavation <kubavation@wp.pl>
2019-08-21 20:28:09 +0200

    init hotellist fragment

--- app/src/main/java/com/example/hotelapp/MainActivity.java
+++ app/src/main/java/com/example/hotelapp/MainActivity.java
@@ -13,8 +13,11 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.hotelapp.fragments.HomeFragment;
+import com.example.hotelapp.fragments.HotelFragmentList;
+import com.example.hotelapp.fragments.dummy.DummyContent;
 
-public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener,
+        HotelFragmentList.OnListFragmentInteractionListener {
 
     private TextView mTextMessage;
 
@@ -29,7 +32,8 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
                     addFragment(new HomeFragment(),false,"s");
                     return true;
                 case R.id.navigation_dashboard:
-                    //mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_dashboard);
+                    addFragment(new HotelFragmentList(),false,"s");
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -61,6 +65,11 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
 
     }
 }
