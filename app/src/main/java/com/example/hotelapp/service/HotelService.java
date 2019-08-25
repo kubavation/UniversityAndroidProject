1c416e174e26ef3faf054d1b5d25c4465002e9d6
kubavation <kubavation@wp.pl>
2019-08-25 13:51:14 +0200

    base method for filtering hotel list

--- app/src/main/java/com/example/hotelapp/service/HotelService.java
+++ app/src/main/java/com/example/hotelapp/service/HotelService.java
@@ -1,18 +1,21 @@
 package com.example.hotelapp.service;
 
+import android.widget.ListView;
+
 import com.example.hotelapp.model.Hotel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class HotelService {
 
     public static List<Hotel> hotels = new ArrayList<>();
 
     static {
-        hotels.add(new Hotel("testowy","desc1",new double[]{51.759445,19.457216},"hotel1"));
-        hotels.add(new Hotel("testowy2","desc2",new double[]{51.759445,19.457216},"hotel2"));
-        hotels.add(new Hotel("testowy3","desc3",new double[]{51.759445,19.457216},"hotel3"));
+        hotels.add(new Hotel("testowy","desc1",new double[]{51.759445,19.457216},"hotel1",50));
+        hotels.add(new Hotel("testowy2","desc2",new double[]{51.759445,19.457216},"hotel2",70));
+        hotels.add(new Hotel("testowy3","desc3",new double[]{51.759445,19.457216},"hotel3",130));
     }
 
     public static Hotel findHotelById(String id) {
@@ -26,6 +29,28 @@ public class HotelService {
     public static void addHotel(Hotel hotel) {
         //+validation
         hotels.add(hotel);
+    }
+
+
+    public static List<Hotel> filterBy(Map<String,Object> filters) {
+
+        List<Hotel> result = new ArrayList<>();
+        //todo add temp list
+        for (Map.Entry<String, Object> filter: filters.entrySet()) {
+
+            //todo cost od / cost do
+            if ( filter.getKey().equals("COST") ) {
+
+                for ( Hotel hotel : hotels) {
+                    if ( hotel.getCostPerPerson() <= (int) filter.getValue() ) {
+                        result.add(hotel);
+                    }
+                }
+            }
+
+        }
+
+        return result;
     }
 
 
